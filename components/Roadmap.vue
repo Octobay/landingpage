0e740196794323adcd71ea30536e7b4201b02179
rikkce <l.riccardo@web.de>
2021-04-05 17:16:59 +0200

    added overflow to prevent mobile horizontol scroll

--- components/Roadmap.vue
+++ components/Roadmap.vue
@@ -1,10 +1,3 @@
-<!--  <vue-timeline-update
-      :date="dataTimeline.date"
-      :description="dataTimeline.description"
-      :title="dataTimeline.title"
-      order="desc"
-    /> -->
-
 <template>
   <div>
     <!-- Latest update -->
@@ -16,29 +9,6 @@
       category="announcement"
       icon="code"
       color="red"
-    />
-
-    <!-- Another update -->
-    <vue-timeline-update
-      :date="new Date('2016-11-22')"
-      title="v2.1.0 - Hunter X Hunter"
-      description="Today I am thrilled to announce the release of Vue.js 2.1.0."
-      thumbnail="/images/vuetimeline/hunter_x_hunter.jpg"
-      category="announcement"
-      icon="code"
-      color="turquoise"
-    />
-
-    <!-- Yet another update -->
-    <vue-timeline-update
-      :date="new Date('2016-09-30')"
-      title="v2.0.0 - Ghost in the Shell"
-      description="Today I am thrilled to announce the release of Vue.js 2.0.0"
-      thumbnail="/images/vuetimeline/ghost_in_the_shell.jpg"
-      category="announcement"
-      icon="code"
-      color="white"
-      is-last
     />
   </div>
 </template>
@@ -110,13 +80,4 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
